e317956747acc30169519695b930d4c4e94dbdce
Seasoned-Programmer <160026389+Seasoned-Programmer@users.noreply.github.com>
2026-05-03 02:15:17 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encodeStr = ""
+        for i in strs:
+            encodeStr = encodeStr+str(len(i))+"ā"+i
+
+        return encodeStr
+            
+            
+        
+
+    def decode(self, s: str) -> List[str]:
+
+        k = 0
+        decodeList = []
+        for i in range(len(s)):
+            if s[i] == "ā":
+
+                size = int(s[k:i])
+                
+                str1 = s[i+1:i+1+size]
+                decodeList.append(str1)
+                i = i+1+size
+                k = i
+                
+        return decodeList
